e3f15410f59b7b0a4ea843238b4c36368f2b1491
Jericho Clare <clarejjr@proton.me>
2023-03-29 21:39:06 +0200

    Aanmeld pagina werkend gemaakt

--- signup.php
+++ signup.php
@@ -1,27 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bilboard top 100</title>
+    <title>Sign Up</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
-<div class="body">
-
-    <body>
-        <div class="popup">
-            <form>
-                <h2>Sign up</h2>
-                <input type="text" placeholder="Username" required>
-                <input type="password" placeholder="Password" required>
-                <input type="password" placeholder="Confirm password" required>
-                <input type="submit" value="Signup">
-                <p>Heb je al een account? <a href="login.html">Log in.</a></p>
-            </form>
-    </body>
-</div>
-
+<body>
+    <div class="popup">
+        <form method="POST">
+            <h2>Sign up</h2>
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="password" name="confirmPassword" placeholder="Confirm password" required>
+            <input type="submit" value="Sign up">
+            <p>Already have an account? <a href="login.html">Log in.</a></p>
+        </form>
+    </div>
+    <?php include("signup-validate.php"); ?>
+</body>
 </html>
