5966c4b9c62555631d9adf47df10354877315f69
Jericho Clare <clarejjr@proton.me>
2023-03-29 21:48:45 +0200

    Aanmeld pagina werkend gemaakt

--- signup.php
+++ signup.php
@@ -16,7 +16,7 @@
             <input type="password" name="password" placeholder="Password" required>
             <input type="password" name="confirmPassword" placeholder="Confirm password" required>
             <input type="submit" value="Sign up">
-            <p>Already have an account? <a href="login.html">Log in.</a></p>
+            <p>Already have an account? <a href="login.php">Log in.</a></p>
         </form>
     </div>
     <?php include("signup-validate.php"); ?>
