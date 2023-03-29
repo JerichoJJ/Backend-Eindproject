009a3e9eb46b3a382f9f232f52717c617018bc11
Jericho Clare~ <clarejjr@gmail.com>
2023-03-29 15:57:40 +0200

    Created first version of the index.php, Signup.php and login.php Page and modified style.css

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bilboard top 100</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+
+<div class="body">
+
+    <body>
+        <div class="popup">
+            <form>
+                <h2>Login</h2>
+                <input type="text" placeholder="Username" required>
+                <input type="password" placeholder="Password" required>
+                <input type="submit" value="Login">
+                <p>Heb je geen account? <a href="signup.html">Maak een account aan.</a></p>
+            </form>
+    </body>
+</div>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bilboard top 100</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<div class="body">
+
+    <body>
+        <div class="popup">
+            <form>
+                <h2>Sign up</h2>
+                <input type="text" placeholder="Username" required>
+                <input type="password" placeholder="Password" required>
+                <input type="password" placeholder="Confirm password" required>
+                <input type="submit" value="Signup">
+                <p>Heb je al een account? <a href="login.html">Log in.</a></p>
+            </form>
+    </body>
+</div>
+
+</html>
